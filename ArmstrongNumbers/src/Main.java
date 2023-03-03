924d9bea77d018dbcbc007876348fc3092a2d355
WorldOfBerk <baris.berk.sengl@hotmail.com>
2023-03-03 18:45:04 +0300

    comment lines added

--- ArmstrongNumbers/src/Main.java
+++ ArmstrongNumbers/src/Main.java
@@ -16,23 +16,23 @@ public class Main {
 
         while(number > 0){
             temp = number%10;
-            sum = sum + (temp * temp *temp); //
+            sum = sum + (temp * temp *temp); // can be written as in mathematical form -> (n*n*n*n*...)
             number = number/10;
         }
-        return sum; //should return to sum for comparing result
+        return sum; //should return to "sum" for comparing result
     }
 
     public static void main(String[] args) {
-        Scanner read = new Scanner(System.in);
+        Scanner read = new Scanner(System.in); // scanner
         int size;
-        int result = 0;
+        int result = 0; // stores "sum" value which comes from calculate_armstrong
 
         System.out.println("How many numbers do you want to enter?");
         size = read.nextInt();
         for(int i = 0; i < size; i++){
             System.out.println("Enter " + (i + 1) + ". three digit element > ");
-            int number = read.nextInt();
-            result = calculate_armstrong(number);
+            int number = read.nextInt(); // takes inputs
+            result = calculate_armstrong(number); // "result" stores "sum" which comes as a return value of function
             if (result == number){
                 System.out.println(number + " => YES");
             }
